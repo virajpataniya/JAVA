12b619edc21b5be610e8a9929eb985dd720cf6bb
virajpataniya <virajpataniya@gmail.com>
2023-12-25 15:54:11 +0530

    Oops intialize

---
+++ OOPS/Oops.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Oops {
+    public static void main(String[] args) {
+        
+    }
+
+    protected static void test(String name){
+        System.out.println("Call from different class");
+    }
+}
