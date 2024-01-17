7418979788e4e1286b6bb65496397d5b7e0d9c8e
virajpataniya <virajpataniya@gmail.com>
2024-01-18 01:32:31 +0530

    Revise some concepts

--- patterns/StarPattern.java
+++ patterns/StarPattern.java
@@ -2,7 +2,82 @@ package patterns;
 
 public class StarPattern {
 
-   /*  public static void hollowRectangle(int row, int col) {
+    public static void butterfly(int n){
+        //1st half
+        for (int i = 1; i<=n; i++) {
+            //stars
+        for (int j =1; j<=i; j++) {
+                System.out.print("*");
+            }
+            //spaces
+        for (int j =1; j <=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            //stars
+        for (int j =1; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();    
+        }
+
+        //2nd Half
+        for (int i=n; i>=1; i--) {
+        //stars
+        for (int j =1; j<=i; j++) {
+                System.out.print("*");
+            }
+        //spaces
+        for (int j =1; j <=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+        //stars
+        for (int j =1; j <=i; j++) {
+                System.out.print("*");
+            }  
+            System.out.println();
+        }
+
+    }
+
+    //Help taken
+    public static void oneZeroTriangle(int n){
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                if ((i+j)%2==0) { 
+                    //even
+                    System.out.print("1");
+                } else {
+                    //odd
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    //Done by myself
+    public static void floydTriangle(int num){
+        int count=1;
+        for (int i = 1; i <=num; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(count);
+                count++;
+            }
+            System.out.println();
+        }
+    }
+
+    //Done by myself
+    public static void invertHalfPyramidNumbers(int nums) {
+        for (int i = 1; i <=nums; i++) {
+            for (int j = 1; j <=nums-i+1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollowRectangle(int row, int col) {
         // HollowRectangle
         //below forloop for rows--horizontal lines
         for (int i = 1; i <= row; i++) {
@@ -16,7 +91,7 @@ public class StarPattern {
             }
             System.out.println();
         }
-    }*/
+    }
 
     public static void invertHalfPyramid(int row){
         for (int i = 1; i <=row; i++) {
@@ -35,7 +110,15 @@ public class StarPattern {
 
     public static void main(String[] args) {
 
-        invertHalfPyramid(4);
+        butterfly(5);
+
+        //oneZeroTriangle(7);
+
+        //floydTriangle(4);
+
+        //invertHalfPyramidNumbers(7);
+
+        //invertHalfPyramid(4);
 
        // hollowRectangle(4, 5);
 
