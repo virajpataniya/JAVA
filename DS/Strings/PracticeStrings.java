113a01251aafc1e9051217e0e869a77604db7a0a
virajpataniya <virajpataniya@gmail.com>
2024-02-21 01:22:43 +0530

    some changes

--- DS/Strings/PracticeStrings.java
+++ DS/Strings/PracticeStrings.java
@@ -78,7 +78,8 @@ public class PracticeStrings {
     public static int countVowels(String str) {
         int count=0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i)=='a'|| str.charAt(i)=='e'|| str.charAt(i)=='i'|| str.charAt(i)=='o' || str.charAt(i)=='u') {
+            char ch=str.charAt(i);
+            if (ch=='a'|| ch=='e'|| ch=='i'|| ch=='o' || ch=='u') {
                 count++;
             }
         }
