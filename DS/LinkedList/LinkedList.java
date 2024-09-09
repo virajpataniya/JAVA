035af3a3a846eae1295b4e9dbc218ed9618efd5b
Viraj <virajpataniya@gmail.com>
2024-09-09 23:59:34 +0530

    Adding little

--- DS/LinkedList/LinkedList.java
+++ DS/LinkedList/LinkedList.java
@@ -223,5 +223,10 @@ public class LinkedList {
         //System.out.println(ll.itrSearch(7));
 
         //System.out.println(ll.recSearch(3));
+        head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=head;
+        System.out.println();
     }
 }
